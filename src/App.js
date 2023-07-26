432f62096c5ddf89ec4ba14cf7bc33acf88efcc3
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-07-26 17:25:34 +0300

    Add files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './index.scss';
+
+function App() {
+  //let count = 0;
+  const [count, setCount] = React.useState(0);
+  
+  const onClickPlus = () => {
+setCount(count + 1);
+  };
+  
+const onClickMinus = () => {
+  setCount(count - 1);
+  };
+
+
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Лічильник:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Мінус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
+      </div>
+    </div> 
+  );
+}
+
+export default App;
